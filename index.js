8ffeba5cc457a58c631453ba4597fd615af2f95b
Kai Suggs <115484419+VorpalMouse@users.noreply.github.com>
2023-08-28 09:43:00 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,2 @@
+/* can use this to change the text of the filter based on how people filter the classrooms (highest-lowest rating, lowest-highest rating, trending or smth idk you get it):
+document.getElementById("filterHead").innerHTML="newtext"; */
